2390eced8709ca1df8bbe618f9fbd8064df2c99c
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-08 14:35:28 +0200

    implement test to write and read 100 tests to the ringbuffer

--- myCode/main.cpp
+++ myCode/main.cpp
@@ -1,19 +1,29 @@
 // Standard (system) header files
 #include <iostream>
 #include <cstdlib>
-// Add more standard header files as required
-// #include <string>
+#include <ctime>
+
+#include "Ringbuffer.h"
 
 using namespace std;
 
-// Add your project's header files here
-// #include "CFraction.h"
-
-// Main program
 int main ()
 {
     // TODO: Add your program code here
 	cout << "ringbuffer-cpp started." << endl << endl;
+    Ringbuffer<int, 100> rb;
 
+    // Seed the random number generator with the current time
+	std::srand(static_cast<unsigned int>(std::time(0)));
+
+	// Generate and print 100 random numbers
+	for (int i = 0; i < 100; ++i)
+	{
+		int randomNumber = std::rand();
+		rb.write(randomNumber);
+		int readNum;
+		rb.read(readNum);
+		cout << "Ringbuffer item number " << i+1 << ": " << readNum << endl;
+	}
 	return 0;
 }
